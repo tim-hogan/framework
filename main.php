b8a5131da984f808472989fd80d4c65b9fd6f80d
tim-hogan <tim@mobilelocate.co.nz>
2021-08-11 18:42:53 +1200

    Update main.php

--- main.php
+++ main.php
@@ -7,6 +7,7 @@
       */ 
 echo "Hello World\n";
 echo "Next change\n";
+echo "Another change\n";
 echo "Before the end we add another commit\n";
 echo "And the ned of this patch for version 1\n";
 ?>
