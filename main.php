d4e847c9302e3891af3be166e2064509d9a2740a
tim-hogan <tim@mobilelocate.co.nz>
2021-08-11 18:15:55 +1200

    Version 2

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+     /*
+      * This this is a test file base version 1.0
+      * 
+      * 
+      * 
+      */ 
+echo "Hello World\n";
+echo "Next change\n";
+echo "Another change\n";
+echo "Before the end we add another commit\n";
+echo "And the ned of this patch for version 1\n";
+?>
